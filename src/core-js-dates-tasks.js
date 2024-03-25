882d9a3212ff1e36cff1233c957f31f535ac9884
Marta109 <marta.hayrapetyan.a@gmail.com>
2024-03-25 16:21:36 +0400

    feat: add task-getCountDaysInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -88,10 +88,6 @@ function getNextFriday(date) {
   return getData;
 }
 
-// getNextFriday('2024-02-03T00:00:00Z');
-// getNextFriday('2024-02-13T00:00:00Z');
-// getNextFriday('2024-02-16T00:00:00Z');
-
 /**
  * Returns the number of days in a specified month and year.
  *
@@ -103,8 +99,11 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month - 1, 1);
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(date.getDate() - 1);
+  return date.getDate();
 }
 
 /**
